98b2d546624abbdd50af7f73685c98d0362b3d26
AbEh <abeh@AbEhs-MacBook-Pro.local>
2023-05-24 08:34:01 -0600

    Added a comment

--- exercises/crewInfo.js
+++ exercises/crewInfo.js
@@ -1,3 +1,7 @@
+// New Changes
+
+
+
 /*
 1. Using for...in, iterate through the spaceship.crew object in the code editor and console.log() a list 
 of crew roles and names in the following format: 
